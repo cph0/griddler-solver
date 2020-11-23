6bd9156f5ff524f422d89371c5ec07ec823e401a
Christian Harrison <christianharrison60@me.com>
2020-11-23 19:29:27 +0000

    hooks & eqeqeq rule

--- Griddlers/ClientApp/src/components/Actions.tsx
+++ Griddlers/ClientApp/src/components/Actions.tsx
@@ -1,34 +1,20 @@
-﻿import * as React from 'react';
-import { RouteComponentProps } from 'react-router';
-import {
-    BarChart, CartesianGrid, XAxis, YAxis,
-    Tooltip, Legend, Bar, ResponsiveContainer
-} from "recharts";
+﻿import React, { useEffect, useState } from "react";
+import { BarChart, CartesianGrid, XAxis, YAxis, Tooltip, Legend, Bar, ResponsiveContainer } from "recharts";
 
 interface ChartData {
     name: string;
     value: number;
 }
 
-interface ActionsState {
-    data: ChartData[];
-}
+export const Actions: React.FunctionComponent = () => {
 
-export class Actions extends React.Component<RouteComponentProps<{}>, ActionsState> {
+    const [data, setData] = useState<ChartData[]>([]);
 
-    constructor(props: any) {
-        super(props);
+    useEffect(() => {
+        get();
+    }, []);
 
-        this.state = {
-            data: []
-        };
-    }
-
-    componentDidMount() {
-        this.get();
-    }
-
-    private get() {
+    const get = () => {
 
         fetch("/Home/GetActionsChart", {
             method: "POST", headers: {
@@ -37,47 +23,42 @@ export class Actions extends React.Component<RouteComponentProps<{}>, ActionsSta
         })
             .then(responce => responce.json() as Promise<any>)
             .then(data => {
-                this.setState({
-                    data
-                });
+                setData(data);
             });
     }
 
+    let html;
 
-    render() {
-        let html;
-
-        html = (
-            <div>
-                <div className="row" style={{ marginTop: "10px", marginBottom: "10px" }}>
-                    <div className="col-md-6">
-                        <div>
-                            <button className="btn btn-primary" onClick={() => this.get()}>Get</button>
-                        </div>
+    html = (
+        <div>
+            <div className="row" style={{ marginTop: "10px", marginBottom: "10px" }}>
+                <div className="col-md-6">
+                    <div>
+                        <button className="btn btn-primary" onClick={() => get()}>Get</button>
                     </div>
                 </div>
-                <div>
-                    <ResponsiveContainer width="100%" height={1000}>
-                        <BarChart
-                            layout="vertical"
-                            data={this.state.data}
-                            margin={{
-                                top: 5, right: 30, left: 20, bottom: 5,
-                            }}
-                        >
-                            <CartesianGrid strokeDasharray="3 3" />
-                            <XAxis type="number" label="Num of Griddlers" />
-                            <YAxis type="category" dataKey="name" label="Action"/>
-                            <Tooltip />
-                            <Legend />
-                            <Bar dataKey="used" fill="#8884d8" />
-                            <Bar dataKey="required" fill="#82ca9d" />
-                        </BarChart>
-                    </ResponsiveContainer>
-                </div>
             </div>
-        );
+            <div>
+                <ResponsiveContainer width="100%" height={1000}>
+                    <BarChart
+                        layout="vertical"
+                        data={data}
+                        margin={{
+                            top: 5, right: 30, left: 20, bottom: 5,
+                        }}
+                    >
+                        <CartesianGrid strokeDasharray="3 3" />
+                        <XAxis type="number" label="Num of Griddlers" />
+                        <YAxis type="category" dataKey="name" label="Action" />
+                        <Tooltip />
+                        <Legend />
+                        <Bar dataKey="used" fill="#8884d8" />
+                        <Bar dataKey="required" fill="#82ca9d" />
+                    </BarChart>
+                </ResponsiveContainer>
+            </div>
+        </div>
+    );
 
-        return html;
-    }
+    return html;
 }
